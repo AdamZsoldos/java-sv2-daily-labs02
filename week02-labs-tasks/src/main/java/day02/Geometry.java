dd9dd175400b5ed37f8fa6bd48f6831a2ab24b07
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-10-19 15:37:41 +0200

    Added day02.Rectangle and Geometry

---
+++ week02-labs-tasks/src/main/java/day02/Geometry.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class Geometry {
+
+    public static void main(String[] args) {
+
+        Rectangle rect = new Rectangle(12.5, 20.0);
+
+        System.out.println(rect.calculateArea());
+    }
+}
